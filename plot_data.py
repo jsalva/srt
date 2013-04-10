73253500b4b9fe581e9344e7233742ad4a6566a2
John Salvatore <jsalva@mit.edu>
2013-04-10 18:28:57 -0400

    improved data visualization

--- plot_data.py
+++ plot_data.py
@@ -1,4 +1,5 @@
 import numpy as np
+import os
 from scipy.ndimage import label
 import matplotlib.pyplot as plt
 from glob import glob
@@ -9,6 +10,7 @@ color = {'S':'blue','T':'green','R':'black','error':'red'}
 
 chunk_size = 8
 for subject_data_file in subject_list:
+    subject = os.path.split(subject_data_file)[-1][:os.path.split(subject_data_file)[-1].find('_data_')]
     time_to_target = []
     fig = plt.figure()
     ax = fig.add_subplot(111)
@@ -21,10 +23,10 @@ for subject_data_file in subject_list:
         block_type = chunk['type'][0]
         labels.append(block_type)
         responded_rt = rt[rt != 0]
-	made_errors = chunk['made_errors']
-	made_errors = made_errors[rt != 0]
-	correct_rt = responded_rt[made_errors==0]
-	error_rt = responded_rt[made_errors==1]
+        made_errors = chunk['made_errors']
+        made_errors = made_errors[rt != 0]
+        correct_rt = responded_rt[made_errors==0]
+        error_rt = responded_rt[made_errors==1]
         ax.plot(np.repeat(idx,len(correct_rt)), correct_rt,'o',color=color[block_type],alpha='0.2',linestyle='None')
         ax.plot(np.repeat(idx,len(error_rt)), error_rt,'x',color=color['error'],linestyle='None')
         ax.plot(idx, np.median(correct_rt),'o',color='black',linestyle='None')
@@ -36,11 +38,49 @@ for subject_data_file in subject_list:
          yerr = [np.std(correct_rt)/np.sqrt(len(correct_rt))],
          ecolor = 'k')
         """
+    
     ind = np.arange(len(labels))
     ax.set_xticks(ind)
     ax.set_xticklabels(labels)   
-    plt.show()
+    ax.set_title(subject)
 
+    
+    errors_vec = data['made_errors']
+    rt_vec = data['rt']
+    cluster_medians = np.zeros(rt_vec.shape)
+    clean_data = data[(errors_vec != 1)&(rt_vec != 0)]
+    types = np.unique(clean_data['type'])
+    cluster_map = dict()
+    for block_type in types:
+        labels = label(clean_data['type'] == block_type)[0]
+        cluster_ids = np.unique(labels)
+        cluster_ids = cluster_ids[cluster_ids != 0]
+        for cluster_id in cluster_ids:
+            cluster_boolean_mask = labels == cluster_id
+            cluster_start_idx = np.nonzero(cluster_boolean_mask)[0][0]
+            cluster_rt = clean_data['rt'][cluster_boolean_mask]
+            print block_type
+            cluster_map[cluster_start_idx] = (block_type,cluster_rt)
+    cluster_order = sorted(cluster_map.keys())
+    skill_scores = []
+    for idx,cluster_idx in enumerate(cluster_order):
+        (block_type,rt) = cluster_map[cluster_idx]
+        if (block_type == 'S') or (block_type == 'T'):
+            median = np.median(rt)
+            flanking_block_median = np.median(np.hstack([
+                    cluster_map[cluster_order[idx-1]][1],
+                    cluster_map[cluster_order[idx+1]][1]
+                    ]))
+            score = (flanking_block_median - median) / flanking_block_median
+            x = cluster_idx/12.
+            y = median
+            skill_scores.append(((x,y),score))
+
+    for location,s_score in skill_scores:
+        plt.text(*location,s='{0:.03f}'.format(s_score))
+
+    plt.show()
+            
 """    mean_sd = np.mean(sd_rts)
     mean_rd = np.mean(rd_rts)
     from scipy.stats import ttest_ind
